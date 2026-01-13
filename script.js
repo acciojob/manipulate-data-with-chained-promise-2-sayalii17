2a15228f80601feff2dfe707f42b2b99a2ade47b
Sayali Patil <137858267+sayalii17@users.noreply.github.com>
2026-01-13 21:16:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
-//your JS code here. If required.
-let op= document.getElementById("output");
+let op = document.getElementById("output");
 
-let arr= [1,2,3,4];
+let arr = [1, 2, 3, 4];
 
 new Promise((resolve) => {
-	setTimeout(() => {
-		resolve(arr);
-	}, 3000);
+  setTimeout(() => {
+    resolve(arr);
+  }, 3000);
 })
 .then((data) => {
-	return new Promise((resolve) => {
-		setTimeout(() => {
-			const evens= data.filter((num) => {num % 2 == 0});
-			op.innerHTML= even.join(",");
-			resolve(evens);
-		}, 1000);
-	});
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const evens = data.filter(num => num % 2 === 0);
+      op.innerHTML = evens.join(",");
+      resolve(evens);
+    }, 1000);
+  });
 })
 .then((evens) => {
-	return new Promise((resolve) => {
-		setTimeout(() => {
-			const double= evens.map((num) => {num*2});
-			op.innerHTML= double.join(",");
-			resolve(double);
-		}, 2000);
-	});
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const doubled = evens.map(num => num * 2);
+      op.innerHTML = doubled.join(",");
+      resolve(doubled);
+    }, 2000);
+  });
 });
