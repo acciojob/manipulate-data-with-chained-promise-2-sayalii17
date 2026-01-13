9ca0fb0b732e228fbe8baff998816a704906ceb0
Sayali Patil <137858267+sayalii17@users.noreply.github.com>
2026-01-13 20:07:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+let op= document.getElementById("output");
+
+let arr= [1,2,3,4];
+
+new Promise((resolve) => {
+	setTimeout(() => {
+		resolve(arr);
+	}, 3000);
+})
+.then((data) => {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			const evens= data.filter((num) => {num % 2 == 0});
+			op.innerHTML= even.join(",");
+			resolve(evens);
+		}, 1000);
+	});
+})
+.then((evens) => {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			const double= evens.map((num) => {num*2});
+			op.innerHTML= double.join(",");
+			resolve(double);
+		}, 2000);
+	});
+});
